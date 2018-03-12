e3c9655fa806fddd9b624d7dbe14d7319819f3bc
toppomameta <toppo850122@gmail.com>
2018-03-13 08:49:51 +0900

    高橋さん指摘：変数削除、コントローラ追記分

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,26 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
-  def index
-    @articles = Article.all
+  def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+      params.require(:article).permit(:title, :body)
+  end
+
 end
